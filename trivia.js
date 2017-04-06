ec59948227aa0730b67f926f8fc3cfae8f9f1919
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-04-06 12:18:07 -0400

    final commit

--- trivia.js
+++ trivia.js
@@ -8,42 +8,58 @@ var incorrect = "incorrect!";
 var questions = [{
     text: "Known for films such as Stand by Me and Running on Empty, this 80's teen idol's career fell short due to an early and unexpected death.",
     answers: ["Michael J Fox", "Ralph Macchio", "Corey Feldman","River Phoenix"],
-    correct: "River Phoenix"
+    correct: "River Phoenix",
+    hint: "Joaquin"
 },
+
 {
     text: "This lead actor earned his first academy award nomination for his role in the film A Street Car Named Desire",
     answers: ["Marlon Brando","Clark Gable","James Dean","James Cagney"],
-    correct: "Marlon Brando"
+    correct: "Marlon Brando",
+    hint: "The Godfather"
 },
+
 {
     text: "In 1993, this film became the most successful film released, beating out the likes of E.T.  This record was later broken by the film Titanic.",
     answers: ["Speed","Forrest Gump","Jurrasic Park","The Lion King"],
-    correct: "Jurrasic Park"
+    correct: "Jurrasic Park",
+    hint: "Steven Spielberg"
 },
+
 {
     text: "Staring in roles such as Cleopatra and Cat on a Hot Tin Roof, this golden era star was known most for her one of a kind violet eyes.",
     answers: ["Vivien Leigh","Audrey Hepburn","Lauren Bacall","Elizabeth Taylor"],
-    correct: "Elizabeth Taylor"
+    correct: "Elizabeth Taylor",
+    hint: "7 husbands 8 marriages"
+
 },
+
 {
     text: "The name Holly Golightly names the lead character for which Golden Era film?",
     answers: ["Some Like It Hot","Breakfast at Tiffanys","Gone With the Wind","Casablanca"],
-    correct: "Breakfast at Tiffanys"
+    correct: "Breakfast at Tiffanys",
+    hint: "Diamonds!"
 },
+
 {
     text: "This 70's horror movie has been credited as the first in a long line of slasher films inspired by Alfred Hitchcock's Psycho.",
     answers: ["The Excorcist","Halloween","The Omen","Black Christmas"],
-    correct: "Halloween"
+    correct: "Halloween",
+    hint: "Haddonfield, Illinois"
 },
+
 {
     text: "This film, starring the legendary Whitney Houston and actor Kevin Costner, has the highest grossing soundtrack of all time selling 17 million copies.",
     answers: ["The Bodyguard","Cinderella","Waiting to Exhale","The Preachers Wife"],
-    correct: "The Bodyguard"
+    correct: "The Bodyguard",
+    hint: "I Will Always Love You"
 },
+
 {
     text: "Judged the greatest film from 1983-2008 by Entertainment Weekly, this film earned Quentin Tarantino his first Academy Award.",
     answers: ["Natural Born Killers","Resevoir Dogs","Pulp Fiction","From Dusk til Dawn"],
-    correct: "Pulp Fiction"
+    correct: "Pulp Fiction",
+    hint: "Royale with cheese"
 }];
 
 function showCurrentQuestion(){
@@ -53,7 +69,10 @@ function showCurrentQuestion(){
     $("#a1").text(currentQuestion.answers[1])
     $("#a2").text(currentQuestion.answers[2])
     $("#a3").text(currentQuestion.answers[3])
+
     isQuestionAnswered = false;
+    $("#help").text("")
+    $("#correctness").text("")
 
 }
 
@@ -73,10 +92,21 @@ function checkAnswer() {
     }
     isQuestionAnswered = true;
     questionIndex++;
+    if(questions.indexOf(currentQuestion) == questions.length - 1) {
+        alert(`Your score is ${score}`)
+        questionIndex = 0
+        showCurrentQuestion()
+    }
+
 }
-// if(questionIndex == question.length - 1)
+
+function helpMe(){
+    $("#help").text(currentQuestion.hint)
+
+}
 
 
 $('#start').on('click', showCurrentQuestion)
 $('.answer-button').on('click', checkAnswer)
 $('#next').on('click', showCurrentQuestion)
+$('#hint').on('click', helpMe)
